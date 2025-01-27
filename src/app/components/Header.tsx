0861b1fad27af8e317b79d8758035002541087b2
Christian <christian-hahn1@gmx.net>
2025-01-27 19:10:15 +0100

    - fixed build error

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Button, Dialog, DialogPanel } from "@headlessui/react";
+import { Dialog, DialogPanel } from "@headlessui/react";
 import {
   Bars3Icon,
   InformationCircleIcon,
