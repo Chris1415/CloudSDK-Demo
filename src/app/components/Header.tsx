6f310953fb447e8d4d96ac22204eb7fd4188bdd6
Christian <christian-hahn1@gmx.net>
2025-02-04 13:22:39 +0100

    - fixed build error

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,7 +9,6 @@ import {
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import ResetSessionButton from "./ResetSessionButton";
-import PopoverEntry from "./PopoverEntry";
 
 const navigation = [
   { name: "Home", href: "/" },
